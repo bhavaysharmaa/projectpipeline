7c2a328a0322edf33b1f250db38c8c06a7009173
bhavaysharma16199 <bhavaysharma16199@gmail.com>
2023-09-07 18:42:56 +0530

    all file commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="Project",
+    version="0.0.1",
+    author="Bhavay Sharma",
+    author_email="bhavaysharma16199@gmail.com",
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'flask']
+)
